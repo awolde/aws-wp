c5bbd749b9cc535868e81d04317e2c7f99160ce4
awolde <amangb@hotmail.com>
2017-07-10 14:14:06 -0500

    latest docker-compose

--- bash-scripts/install-docker.sh
+++ bash-scripts/install-docker.sh
@@ -14,7 +14,7 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install docker-ce -y
-sudo sh -c 'curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose'
+sudo sh -c 'curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose'
 sudo chmod +x /usr/local/bin/docker-compose
 sudo groupadd docker
 sudo usermod -aG docker $USER
